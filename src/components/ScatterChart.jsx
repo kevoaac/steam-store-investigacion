9d53e84c5b926b3ac58c4b8cf772c208ccf9c378
kevoaac <130163890+kevoaac@users.noreply.github.com>
2024-01-30 23:59:13 -0500

    ShadowItem, Scatter

---
+++ src/components/ScatterChart.jsx
@@ -0,0 +1,33 @@
+import { Scatter } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
+import { useState } from "react";
+import { ScatterData } from "../mocks/data";
+
+function ScatterChart({ tailwindStyles }) {
+  const [scatterData, setScatterData] = useState({
+    labels: ScatterData.map((data) => data.x),
+    datasets: [
+      {
+        label: "Users Gained",
+        data: ScatterData.map((data) => data.y),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  });
+
+  return (
+    <div className={`w-full ${tailwindStyles}`}>
+      <Scatter data={scatterData} />
+    </div>
+  );
+}
+
+export default ScatterChart;
